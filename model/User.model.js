65e987d22bbad486dce95139e85107992dab3334
Izaan Ahmed Khan <izaanahmedkhan@gmail.com>
2023-09-06 19:19:20 +0500

    User model

---
+++ model/User.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+let userSchema = new Schema({
+    name: { type: String },
+    email: { type: String },
+    password: { type: String },
+});
+
+userSchema.pre('save', async function (next) {
+    if(this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next(); 
+});
+
+module.exports = mongoose.model('User', userSchema);
