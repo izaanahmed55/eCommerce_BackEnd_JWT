829079b02ce71451afd6485719bd3e767f9018f1
abrar-11 <abrarahmed2185@gmail.com>
2023-09-14 01:54:40 +0500

    Updated Code

--- controller/User.controller.js
+++ controller/User.controller.js
@@ -2,120 +2,132 @@ const User = require("../model/User.model");
 const { signAccessToken, verifyAccessToken } = require("../services/jwt");
 
 const userSignUp = async (req, res, next) => {
-    const { username, email, password} = req.body;
+  const { username, email, password } = req.body;
 
-    try {
-        const emailInUse = await User.exists({ email });
+  try {
+    const emailInUse = await User.exists({ email });
 
-        const usernameInUse = await User.exists({ username });
+    const usernameInUse = await User.exists({ username });
 
-        if (emailInUse) {
-            const error = {
-                status: 409,
-                message: "Email already registered, use another email!",
-            };
+    if (emailInUse) {
+      const error = {
+        status: 409,
+        message: "Email already registered, use another email!",
+      };
 
-            return next(error);
-        }
-
-        if (usernameInUse) {
-            const error = {
-                status: 409,
-                message: "Username not available, choose another username!",
-            };
-
-            return next(error);
-        }
-
-    } catch (error) {
-        return next(error);
+      return next(error);
     }
 
-    try {
-        const user = await User.create({
-            username,
-            email,
-            password,
-        });
+    if (usernameInUse) {
+      const error = {
+        status: 409,
+        message: "Username not available, choose another username!",
+      };
 
-        const accessToken = signAccessToken({ _id: user._id }, "10m");
-
-        // send tokens in cookie
-        res.cookie("accessToken", accessToken, {
-            maxAge: 1000 * 60 * 60 * 24,
-        });
-
-        return res.status(201).json({ user: user, auth: true });
-
-    } catch (error) {
-        const err = error;
-        res.status(400).send(err);
+      return next(error);
     }
+  } catch (error) {
+    return next(error);
+  }
+
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+
+    const accessToken = signAccessToken({ _id: user._id }, "10m");
+
+    // send tokens in cookie
+    res.cookie("accessToken", accessToken, {
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    return res.status(201).json({ user: user, auth: true });
+  } catch (error) {
+    const err = error;
+    res.status(400).send(err);
+  }
 };
 
 const userSignIn = async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        const user = await User.findOne({ email });
+  try {
+    const user = await User.findOne({ email });
 
-        if (!user) {
-            const error = {
-                status: 409,
-                message: "User not Found!",
-            };
+    if (!user) {
+      const error = {
+        status: 409,
+        message: "User not Found!",
+      };
 
-            return next(error);
-        }
-
-        const accessToken = signAccessToken({ _id: user._id }, "10m");
-
-        console.log('user: ', user)
-
-        res.cookie("accessToken", accessToken, {
-            maxAge: 1000 * 60 * 60 * 24,
-            httpOnly: true,
-        });
-
-        return res.status(201).json({ user: user, auth: true, accessToken});
-
-    } catch (error) {
-        return next(error);
+      return next(error);
     }
 
+    const accessToken = signAccessToken({ _id: user._id }, "10m");
+
+    // console.log("user: ", user);
+
+    res.cookie("accessToken", accessToken, {
+      maxAge: 1000 * 60 * 60 * 24,
+      httpOnly: true,
+    });
+
+    console.log("accessToken set:", accessToken);
+
+    return res.status(201).json({ user: user, auth: true, accessToken });
+  } catch (error) {
+    return next(error);
+  }
 };
 
 const isLoggedIn = async (req, res, next) => {
-    try {
-        // Access Token Validation
-        const { accessToken } = req.cookies;
+  try {
+    // Access Token Validation
+    const { accessToken } = req.cookies;
 
-        console.log("Access Token: ", req.cookies);
+    console.log("Cookies: ", req.cookies, " 🔰 Access token : ", accessToken);
 
-        if (!accessToken) {
-            return res.json({auth: false, isLogin: false})
-        }
-
-        let _id;
-
-        try {
-            const decodedToken = verifyAccessToken(accessToken);
-            _id = decodedToken._id;
-            console.log(_id)
-            res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-            return res.json({auth: true, isLogin: true})
-        } catch (error) {
-            return next(error);
-        }
-
-
-    } catch (error) {
-        return next(error);
+    if (!accessToken) {
+      console.log("Access token Undefined");
+      return res.json({ auth: false, isLogin: false });
     }
+
+    let _id;
+
+    try {
+      const decodedToken = verifyAccessToken(accessToken);
+      _id = decodedToken._id;
+      console.log(_id);
+
+      return res.json({ auth: true, isLogin: true });
+    } catch (error) {
+      return next(error);
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+const logOut = async (req, res, next) => {
+  try {
+    res.clearCookie("accessToken", { maxAge: 0 });
+    const { accessToken } = req.cookies;
+    console.log("Logout Cookies: ", req.cookies, " 🔰 Access token : ", accessToken);
+    res.status(200).json({
+      auth: false,
+      isLogin: false,
+      message: "Access token cookie deleted",
+    });
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports = {
-    userSignUp, 
-    userSignIn,
-    isLoggedIn,
+  userSignUp,
+  userSignIn,
+  isLoggedIn,
+  logOut,
 };
--- routes/User.routes.js
+++ routes/User.routes.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const { userSignUp, userSignIn, isLoggedIn} = require('../controller/User.controller')
-const { authenticate } = require('../middleware/authenticate')
+const express = require("express");
+const {
+  userSignUp,
+  userSignIn,
+  isLoggedIn,
+  logOut,
+} = require("../controller/User.controller");
+const { authenticate } = require("../middleware/authenticate");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/profile', authenticate, (req, res) => {
-    res.json(req.user)
-})
-router.post('/signup', userSignUp)
-router.post('/signin', userSignIn)
-router.post('/isloggedin', isLoggedIn)
+router.post("/profile", authenticate, (req, res) => {
+  res.json(req.user);
+});
+router.post("/signup", userSignUp);
+router.post("/signin", userSignIn);
+router.post("/isloggedin", isLoggedIn);
+router.post("/logOut", logOut);
 
 module.exports = router;
